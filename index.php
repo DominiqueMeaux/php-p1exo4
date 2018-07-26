705c2aee1dc61240ca9bcf0e99503e21e69256f1
dominiquem <dominique.meaux@gmail.com>
2018-07-26 09:23:02 +0200

    php p1exo4

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8"/>
+    <title>p1exo4</title>
+  </head>
+  <body>
+    <p>
+      <?php
+        $name = 'dominique';
+        echo $name;
+      ?>
+    </p>
+    <p>
+      <?php
+        $age=34;
+          echo $age;
+      ?>
+    </p>
+    <p>
+      <?php
+        $poids = 73.2;
+          echo $poids;
+      ?>
+    </p>
+    <p>
+      <?php
+
+        
+
+      ?>
+    </p>
+  </body>
+</html>
